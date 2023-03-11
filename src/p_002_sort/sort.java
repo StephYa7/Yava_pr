fe185a673c0d73946d2ebbdf7ea5b953ad446a6f
StephYa7 <1000p@mail.ru>
2023-03-11 11:17:54 +0700

    +insertSort

--- src/p_002_sort/sort.java
+++ src/p_002_sort/sort.java
@@ -25,6 +25,16 @@ public class sort {
                 , 4, 2, 5, 6, 7, 45, 546, 47, 458, 45, 56, 2452, 36, 647, 75, 67, 56, 75, 6
                 , 7567, 56756, 7, 56, 3, 45, 5, 634, 5, 99, 5, 31, 5, 6, 7, 8, 4, 2, 1, 6, 55, 2, 31
                 , 4};
+        int[] array3 = new int[]{1, 5, 6, 7, 8, 4, 2, 1, 6, 55, 2, 31
+                , 4, 2, 5, 6, 7, 45, 546, 47, 4, 58, 45, 56, 24, 52, 36, 647, 75, 67, 56, 75, 6
+                , 7567, 56756, 7, 56, 3, 45, 5, 63, 4, 5, 99, 5, 31, 5, 6, 7, 8, 4, 2, 1, 6, 55, 2, 31
+                , 4, 2, 5, 6, 7, 45, 546, 47, 458, 4, 556, 2452, 36, 647, 75, 67, 56, 75, 6
+                , 7567, 56756, 7, 56, 3, 45, 5, 63, 4, 5, 99, 5, 31, 5, 6, 7, 8, 4, 2, 1, 6, 55, 2, 31
+                , 4, 2, 5, 6, 7, 45, 546, 47, 458, 45, 56, 2452, 36, 647, 75, 67, 56, 75, 6
+                , 7567, 56756, 7, 56, 3, 45, 5, 63, 4, 5, 99, 5, 31, 5, 6, 7, 8, 4, 2, 1, 6, 55, 2, 31
+                , 4, 2, 5, 6, 7, 45, 546, 47, 458, 45, 56, 2452, 36, 647, 75, 67, 56, 75, 6
+                , 7567, 56756, 7, 56, 3, 45, 5, 634, 5, 99, 5, 31, 5, 6, 7, 8, 4, 2, 1, 6, 55, 2, 31
+                , 4};
 
         AtomicInteger counter = new AtomicInteger();
         long startTime = System.nanoTime();
@@ -35,7 +45,7 @@ public class sort {
         System.out.printf("Пузырьковая сортировка. Колличество итерраций: %d", counter.get());
         long endTime = System.nanoTime();
         long duration = (endTime - startTime);
-        System.out.printf("    Время процесса 1: %d \n", duration/1000);
+        System.out.printf("    Время процесса 1: %d \n", duration / 1000);
 
 
         counter.set(0);
@@ -45,11 +55,22 @@ public class sort {
 //        for (int i = 0; i < array.length; i++) {
 //            System.out.println(array[i]);
 //        }
-
         System.out.printf("Сортировка выбором. Колличество итерраций: %d", counter.get());
         endTime = System.nanoTime();
         duration = (endTime - startTime);
-        System.out.printf("    Время процесса 2: %d \n", duration/1000);
+        System.out.printf("    Время процесса 2: %d \n", duration / 1000);
+
+        counter.set(0);
+        startTime = System.nanoTime();
+
+        insertSort(array3, counter);
+//        for (int i = 0; i < array.length; i++) {
+//            System.out.println(array[i]);
+//        }
+        System.out.printf("Сортировка вставками. Колличество итерраций: %d", counter.get());
+        endTime = System.nanoTime();
+        duration = (endTime - startTime);
+        System.out.printf("    Время процесса 2: %d \n", duration / 1000);
     }
 
 
@@ -87,6 +108,19 @@ public class sort {
                 int temp = array[i];
                 array[i] = array[minPosition];
                 array[minPosition] = temp;
+            }
+        }
+    }
+
+    public static void insertSort(int[] array, AtomicInteger counter) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    counter.incrementAndGet();
+                }
             }
         }
     }
